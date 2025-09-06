f710cf626600df6f9307c3003c846fd78b13f8f4
duli <123609742+drusom@users.noreply.github.com>
2025-09-05 23:36:42 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()], 
+  base: '/threejs-visualizer-/',
   server: {
     host: '0.0.0.0',
     port: 3092,
   },
+
 })
